7fac7d067d527ce1b5c4f96dd4705a0693424d9d
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2025-02-09 17:56:48 +0530

    Added live chat page ui

--- app/dashboard/new-chat/page.tsx
+++ app/dashboard/new-chat/page.tsx
@@ -3,7 +3,7 @@ import ChatUI from "@/components/chat/chat-ui";
 
 export default function Page() {
   return (
-    <div className="w-full  h-full bg-app-secondary flex flex-col rounded-lg pb-10 px-4">
+    <div className="w-full  h-full bg-app-secondary flex flex-col rounded-lg pb-5 px-4">
       <ChatUI />
       <ChatInput />
     </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,15 @@
 import Header from "@/components/dashboard/header";
 import MainSection from "@/components/dashboard/main-section";
+import LiveChatUI from "@/components/live-chat/live-chat-ui";
 
 export default function Page() {
   return (
-    <div className="p-9 w-full flex-1 bg-app-secondary rounded-lg flex flex-col gap-4">
-      <Header />
-      <MainSection />
+    <div>
+      <LiveChatUI />
     </div>
   );
+  /**<div className="p-9 w-full flex-1 bg-app-secondary rounded-lg flex flex-col gap-4">
+      <Header />
+      <MainSection />
+    </div> */
 }
